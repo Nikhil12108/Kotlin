fae8ee6c6f39357ee9b96247f649928894d64520
Nikhil Kumar <nikhilkumar84157@gmail.com>
2024-10-14 10:20:57 +0530

    completed functions and added classes

--- src/functions.kt
+++ src/functions.kt
@@ -9,8 +9,21 @@ fun hello() {
 fun sum(x:Int, y:Int) = x + y
 fun main() {
     hello()
-    println(sum(1,2))
-    val upperCaseString = { text : String -> text.uppercase()}
+    println(sum(1, 2))
+    val upperCaseString = { text: String -> text.uppercase() }
     println(upperCaseString("hello"))
+
+// pass lambda function into another function
+    val number = listOf(1, -2, 3, -4, 5, -6)
+
+    val positives = number.filter { x -> x > 0 }
+    val isNegative = {x:Int -> x < 0}
+    val negatives = number.filter(isNegative)
+
+    println(positives)
+    println(negatives)
 }
-// lamba function
+
+
+
+
